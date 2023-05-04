e24d4ae9cdb33dc747d82909c39612d475c77fb8
mehdi-infostrux <112959205+mehdi-infostrux@users.noreply.github.com>
2023-05-04 12:43:19 -0300

    Update classification_update.py

--- classification_update.py
+++ classification_update.py
@@ -43,9 +43,8 @@ with st.form("data_editor_form"):
 
 if submit_button:
     try:
-        session.write_pandas(edited, "CUSTOMERS", overwrite=False)
+        session.write_pandas(edited, "CUSTOMERS", overwrite=True)
         st.success("Table updated")
-        st.dataframe(edited)
         time.sleep(5)
     except:
         st.warning("Error updating table")
